1ecb02a5c80436f328ff6ec5e9056266d98c80e9
Bryan Forbes <bryan@reigndropsfall.net>
2014-03-21 11:31:05 -0500

    Tweak to environment configs

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,1 +1,6 @@
 export DISPLAY=":0.0"
+
+# Add sensitive information to ~/.zshlocalrc
+if [ -e "$HOME/.zshlocalrc" ]; then
+	source "$HOME/.zshlocalrc"
+fi
--- zsh/local.zsh
+++
@@ -1,5 +0,0 @@
-# Add sensitive information here (this file is ignored)
-export GITHUB_USER=""
-export GITHUB_TOKEN=""
-export SAUCE_USERNAME=""
-export SAUCE_ACCESS_KEY=""
